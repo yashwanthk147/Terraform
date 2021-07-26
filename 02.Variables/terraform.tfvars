883b458b6e3de98b77474a65aeb37c3e701fd4cd
MYPC <yashwanthkotha147@gmail.com>
2021-07-26 19:29:55 +0530

    Demonistrating, How the git repos will work.

---
+++ 02.Variables/terraform.tfvars
@@ -0,0 +1,1 @@
+FRIUTS = ["Bannana", "Apple"]
--- 02.Variables/vars.tf
+++ 02.Variables/vars.tf
@@ -34,32 +34,12 @@ output "boolean" {
 }
 
 #Multiple variable types
-variable "file45" {
-  default = "movie-list"
-}
-variable "file65" {
-  default = [
-  "Toofan", 100 , "PIG"]
-}
-variable "file78" {
-  default = { film = "mafia" ,
-              price = 65
-  }
-}
-output "file45" {
-   value = "var.movie-list"
-}
-output "file65-first" {
-  value = "var.file65[0]"
-}
-output "file65-second" {
-  value = "var.file65[1]"
-}
-output "file65-third" {
-  value = "var.file65[2"
-}
-output "file78" {
-  value = "films = ${var.file78[0]}"
+
+#tfvars
+
+variable "FRIUTS" {}
+output "FRIUTS" {
+  value = "FRIUTS"
 }
 
 
