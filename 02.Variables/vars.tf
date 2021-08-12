e2842af2821a6dbc828f6a9eb66a4523de9d519b
MYPC <yashwanthkotha147@gmail.com>
2021-08-12 17:04:56 +0530

    files are added.

--- 02.Variables/vars.tf
+++ 02.Variables/vars.tf
@@ -63,3 +63,16 @@ output "enemy5659" {
   value = var.enemy5659
 }
 
+variable "string" {
+  default = "hEllo"
+}
+variable "number" {
+  default = 100
+}
+variable "boolean" {
+  default = True
+}
+output "data-types" {
+  value = "string=${var.string}, number=${var.number}, boolean=${var.boolean}"
+}
+
