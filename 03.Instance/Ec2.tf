fea307167763457b431b8660c4a34e39a62cd10b
MYPC <yashwanthkotha147@gmail.com>
2021-08-12 19:04:47 +0530

    files are added.

--- 03.Instance/Ec2.tf
+++ 03.Instance/Ec2.tf
@@ -1,8 +1,39 @@
-resource "aws_instance" "sample" {
-  ami           = "ami-074df373d6bafa625"
-  instance_type = "t2.micro"
+//resource "aws_instance" "sample" {
+//  ami                    = "ami-074df373d6bafa625"
+//  instance_type          = "t2.micro"
+//  vpc_security_group_ids =
+//
+//  tags                   = {
+//    Name                 = "sample"
+//  }
+//}
 
-  tags          = {
-    Name        = "sample"
+resource "aws_security_group" "allow_ssh" {
+  name                   = "allow_ssh"
+  description            = "allow_ssh"
+
+
+  ingress {
+  description            = "SSH"
+  from_port              = 22
+  to_port                = 22
+  protocol               = "tcp"
+  cidr_blocks            = ["0.0.0/0"]
   }
+
+  egress{
+  from_port              = 0
+  to_port                = 0
+  protocol               = "-1"
+  cidr_blocks            = ["0.0.0.0/0"]
+  }
+
+
+  tags                   = {
+  Name                   = "allow_ssh"
+  }
+}
+
+output "sg_attributes" {
+  value= aws_security_group.allow_ssh
 }
