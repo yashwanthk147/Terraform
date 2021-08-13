4ff7a66529b7cb19b3612285305f8cdbe54e8861
MYPC <yashwanthkotha147@gmail.com>
2021-08-13 11:33:14 +0530

    files are added.

---
+++ 06.Conditions/sample.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "sample" {
+  count                  = var.create ? 1 : 0
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t2.micro"
+  #vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags                   = {
+    Name                 = "sample"
+  }
+}
+
+provider "aws"{
+  region           = "us-east-1"
+}
+
+variable "create" {
+  default = 2
+}
