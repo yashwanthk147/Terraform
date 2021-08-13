99cf9a2761c5379c03d3d0206a85a028e73912fe
MYPC <yashwanthkotha147@gmail.com>
2021-08-13 11:17:57 +0530

    files are added.

--- 05.Modules/ec2/ec2.tf
+++ 05.Modules/ec2/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "sample" {
+  count                  =  2
   ami                    = "ami-074df373d6bafa625"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.SG_ID]
 
   tags                   = {
-    Name                 = "Sxample"
+    Name                 = "Sxample-$(count.index)"
   }
 }
 
