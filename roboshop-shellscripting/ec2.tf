ce00efd2f695909670f9f4c74d5f96c1e3d54228
MYPC <yashwanthkotha147@gmail.com>
2021-08-16 16:21:11 +0530

    files are added.

--- roboshop-shellscripting/ec2.tf
+++ roboshop-shellscripting/ec2.tf
@@ -9,28 +9,37 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "aws_route53_record" "records" {
+  count                 = local.LENGTH
+  name                  = element("var.COMPONENTS", count.index )
+  type                  = "A"
+  zone_id               = "Z0503812D1634TD1MLB0"
+  ttl                   = 300
+  records               = [element(aws_instance.instance.*.private_ip, count.index)]
+}
+
 #1now i want to connect to the instance and clone the repos and run some commands as well inside the shell scripting
 #2to connect to the instance i use resource but resource can do only some connection. but, Actual connection done by provisioner
 #3null_resource is a resource type which does nothing
-resource "null_resource" "run-shell-scripting" {
-  count                = local.LENGTH
-  provisioner "remote-exec" {
-
-    connection {
-      host            = element(aws_instance.instance.*.public_ip, count.index)  #ineed to connect to the public ip of the instance, how do i connect? using element
-      user            ="centos"
-      password        = "DevOps321"
-
-    }
-  }
-    inline           = [
-      "cd/home/centos",
-      "git clone https://github.com/yashwanthk147/Shell-Scripting.git",
-      "cd Shell-Scripting/roboshop",
-      "sudo make ${element(var.COMPONENTS, count.index)}"
-    ]
-  }
-
+//resource "null_resource" "run-shell-scripting" {
+//  count                = local.LENGTH
+//  provisioner "remote-exec" {
+//
+//    connection {
+//      host            = element(aws_instance.instance.*.public_ip, count.index)  #ineed to connect to the public ip of the instance, how do i connect? using element
+//      user            ="centos"
+//      password        = "DevOps321"
+//
+//    }
+//  }
+//    inline           = [
+//      "cd/home/centos",
+//      "git clone https://github.com/yashwanthk147/Shell-Scripting.git",
+//      "cd Shell-Scripting/roboshop",
+//      "sudo make ${element(var.COMPONENTS, count.index)}"
+//    ]
+//  }
+//
 locals {
   LENGTH             = length(var.COMPONENTS)
 }
