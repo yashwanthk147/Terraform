b43eefb0a4f5c2a03de8cc8e0efce09c6de9c295
MYPC <yashwanthkotha147@gmail.com>
2021-08-17 15:38:49 +0530

    files are added.

--- 07.data-sources/data.tf
+++ 07.data-sources/data.tf
@@ -1,11 +1,10 @@
 data "aws_ami" "example" {
-  #executable_users = ["self"]
   most_recent      = true
   name_regex       = "Centos-7"
-  owners           = [973714476881]
+  owners           = ["973714476881"]
 
 }
 
-output "ami" {
+output "amis" {
   value = "data.aws_ami.example"
 }
