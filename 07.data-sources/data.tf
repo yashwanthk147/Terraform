6c478e49e46ea1f87b5acf5ef84f7d5c8243b8e1
MYPC <yashwanthkotha147@gmail.com>
2021-08-17 17:56:31 +0530

    files are added.

--- 07.data-sources/data.tf
+++ 07.data-sources/data.tf
@@ -4,17 +4,6 @@ data "aws_ami" "example" {
   owners           = ["973714476881"]
 }
 
-data "aws_ec2_local_gateways" "example" {
-  tags = {
-    service = "production"
-  }
-}
-
 output "amis" {
   value = data.aws_ami.example.id
-}
-
-
-output "example" {
-  value = data.aws_ec2_local_gateways.example.ids
 }
